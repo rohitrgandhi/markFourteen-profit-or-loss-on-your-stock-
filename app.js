0edd128718bfdd8274b30155ee76d4a52eee225b
Rohit Gandhi <rohit.r.gandhi@gmail.com>
2021-09-21 00:02:11 +0530

    Updated app.js for 'NaN' error

--- app.js
+++ app.js
@@ -9,36 +9,34 @@ var output=document.querySelector("#output");
 var calculateButton=document.querySelector("#button");
 
 //when button is clicked calculate function is called.
-calculateButton.addEventListener("click", calculate);
+calculateButton.addEventListener("click", calculatePNL);
 
-function calculate()
-{  
-    buy=Number(buy.value);
-    cmp=Number(cmp.value);
-    qty=Number(qty.value);
-    var abs;
-    var percent;
+
+function calculatePNL(){
+    var CP=Number(buy.value);
+    var SP=Number(cmp.value);
+    var Q=Number(qty.value);
+    calculate(CP,Q,SP);
+}
+
+function calculate(buy,qty,cmp)
+{ 
     if(cmp>buy)
     {
-        var profit=cmp-buy;
-        var totalProfit=profit*qty;
-        var profitPercentage=(profit/buy)*100;
+        var totalProfit=(cmp-buy)*qty;
+        var profitPercentage=(totalProfit/buy)*100;
         output.style.color="green"; //bonus
         outputMsg(`Hurray...Total Profit is Rs.${totalProfit} and the Profit Percentage is ${profitPercentage.toFixed(2)}%`)//bonus
     }
     else if(cmp<buy)
     {   
-        var loss=buy-cmp;
-        abs=loss*qty;
-        percent=(loss/buy)*100;
+        var totalLoss=(buy-cmp)*qty;
+        var lossPercentage=(totalLoss/buy)*100;
         output.style.color="red";//bonus
-        outputMsg(`Unfortunately, Loss is Rs.${abs} and the percent is ${percent.toFixed(2)}%`)//bonus
+        outputMsg(`Unfortunately, Loss is Rs.${totalLoss} and the percent is ${lossPercentage.toFixed(2)}%`)//bonus
     }
     else
     {
-        var diff=buy-cmp;
-        var abs=diff*qty;
-        var percent=(diff/buy)*100;
         outputMsg(`No Profit No Loss`)
     }
 }
