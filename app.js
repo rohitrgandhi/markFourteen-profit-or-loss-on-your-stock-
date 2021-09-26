1b5c796033d9e87779a077c6e3cc4a963a8d30eb
Rohit Gandhi <rohit.r.gandhi@gmail.com>
2021-09-27 00:49:25 +0530

    fix: no input error message

--- app.js
+++ app.js
@@ -20,27 +20,30 @@ function calculatePNL(){
 }
 
 function calculate(buy,qty,cmp)
-{ 
-    if(cmp>buy)
+{   
+    if(buy>0 && qty>0 && cmp>0)
     {
-        var totalProfit=(cmp-buy)*qty;
-        var profitPercentage=(totalProfit/buy)*100;
-        output.style.color="green"; //bonus
-        outputMsg(`Hurray...Total Profit is Rs.${totalProfit} and the Profit Percentage is ${profitPercentage.toFixed(2)}%`)//bonus
+        if(cmp>buy)
+        {
+            var totalProfit=(cmp-buy)*qty;
+            var profitPercentage=(totalProfit/buy)*100;
+            output.style.color="green"; //bonus
+            outputMsg(`Hurray...Total Profit is Rs.${totalProfit} and the Profit Percentage is ${profitPercentage.toFixed(2)}%`)//bonus
+        }
+        else if(cmp<buy)
+        {   
+            var totalLoss=(buy-cmp)*qty;
+            var lossPercentage=(totalLoss/buy)*100;
+            output.style.color="red";//bonus
+            outputMsg(`Unfortunately, Loss is Rs.${totalLoss} and the percent is ${lossPercentage.toFixed(2)}%`)//bonus
+        }
+        else
+        {   output.style.color="#00BFA6";
+            outputMsg(`No Profit No Loss`)
+        }
     }
-    else if(cmp<buy)
-    {   
-        var totalLoss=(buy-cmp)*qty;
-        var lossPercentage=(totalLoss/buy)*100;
-        output.style.color="red";//bonus
-        outputMsg(`Unfortunately, Loss is Rs.${totalLoss} and the percent is ${lossPercentage.toFixed(2)}%`)//bonus
-    }
-    else
-    {
-        outputMsg(`No Profit No Loss`)
-    }
+    else alert('Please fill out all Fields');
 }
-
 
 function outputMsg(message)
 {   output.innerHTML=message;
